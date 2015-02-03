b44a8fc6410195ff205804b8fff422dc9335c126
Sergio Marin <higher.vnf@gmail.com>
2015-02-02 20:48:14 -0430

    Clase 9

--- clases/clase8/js/app.js
+++ clases/clase8/js/app.js
@@ -2,7 +2,23 @@
 
 $(document).ready(function() {
   
-  $(".galeria").fancybox();
+  $(".galeria").fancybox({
+    'helpers': {
+      'title': {
+        'type': 'outside'
+      },
+      'buttons': {
+        'position': 'bottom'
+      },
+      'thumbs': {
+        'width': 100,
+        'height': 100,
+        'position': 'top'
+      }
+    }
+  });
+  
+  
   $("#boton1").click(function() {
     $("body").toggleClass("fondo-negro");
   });
